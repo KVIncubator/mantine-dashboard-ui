b6971e16a65e3835b84a7486aef93b5b03186275
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2024-09-17 09:47:52 +0300

    style: fixed by prettier:write

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,19 +1,19 @@
-import {combineReducers, AnyAction, Reducer} from 'redux'
-import auth, {AuthState} from './slices/auth'
-import base, {BaseState} from './slices/base'
-import locale, {LocaleState} from './slices/locale/localeSlice'
-import theme, { ThemeState } from './slices/theme/themeSlice'
+import { combineReducers, AnyAction, Reducer } from 'redux';
+import auth, { AuthState } from './slices/auth';
+import base, { BaseState } from './slices/base';
+import locale, { LocaleState } from './slices/locale/localeSlice';
+import theme, { ThemeState } from './slices/theme/themeSlice';
 
 export type RootState = {
-  auth: AuthState
-  base: BaseState
-  locale: LocaleState
-  theme: ThemeState
+  auth: AuthState;
+  base: BaseState;
+  locale: LocaleState;
+  theme: ThemeState;
   /* eslint-disable @typescript-eslint/no-explicit-any */
-}
+};
 
 export interface AsyncReducers {
-  [key: string]: Reducer<any, AnyAction>
+  [key: string]: Reducer<any, AnyAction>;
 }
 
 const staticReducers = {
@@ -21,15 +21,14 @@ const staticReducers = {
   base,
   locale,
   theme,
-}
+};
 
-const rootReducer =
-  (asyncReducers?: AsyncReducers) => (state: RootState, action: AnyAction) => {
-    const combinedReducer = combineReducers({
-      ...staticReducers,
-      ...asyncReducers,
-    })
-    return combinedReducer(state, action)
-  }
+const rootReducer = (asyncReducers?: AsyncReducers) => (state: RootState, action: AnyAction) => {
+  const combinedReducer = combineReducers({
+    ...staticReducers,
+    ...asyncReducers,
+  });
+  return combinedReducer(state, action);
+};
 
-export default rootReducer
+export default rootReducer;
